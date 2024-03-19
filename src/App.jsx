aea888d701141263de532dc33df402a93982c448
nickncb96 <nicholasncb12@hotmail.com>
2024-03-19 20:59:50 +0000

    updating the pages folder to connect to the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
-import Signup from './Components/Pages/Signup';
-import Home from './Components/Pages/Home';
-import Login from './Components/Pages/Login';
-import Explore from './Components/Pages/Explore';
+import Signup from './pages/signup';
+import Login from './pages/login';
+import Explore from './pages/explore';
 import './App.css';
+import Home from './pages/home';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <div className="content">
-          <Routes>
-            <Route path="/" component={<Home />} />
-            <Route path="/signup" component={<Signup />} />
-            <Route path="/login" component={<Login />} />
-            <Route path="/explore" component={<Explore />} />
-          </Routes>
-        </div>
+
+    <div>
+      <Navbar />
+      <div className="content">
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/explore" element={<Explore />} />
+        </Routes>
+
       </div>
-    </Router>
+    </div>
   );
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,9 +9,10 @@ const Navbar = () => {
                 <Typography variant="h6" style={{ flexGrow: 1 }}>
                     <Link to="/">Small Eventz</Link>
                 </Typography>
-                <Button component={Link} to="/signup" color="inherit">Signup</Button>
-                <Button component={Link} to="/login" color="inherit">Login</Button>
                 <Button component={Link} to="/explore" color="inherit">Explore</Button>
+                <Button component={Link} to="/login" color="inherit">Login</Button>
+                <Button component={Link} to="/signup" color="inherit">Signup</Button>
+                
             </Toolbar>
         </AppBar>
     );
