efb1fcf5cd41197891eea3c0a7bd54827c35423d
Betelgeuse <abdumavlyanov2004@yahoo.com>
2024-03-19 12:49:21 +0000

    with googlemap and weather api and events code to test apis

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-
+// import './App.css'
+import MapTool from './Components/Map/Map2'
+import Events from './Components/Events/Events'
 function App() {
-  const [count, setCount] = useState(0)
+  const [latlon, setlatlon] = useState({lat:0, lon:0});
+
+  function getLatLon (address) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + address + "&units=metric&appid=965f5c48346a9c64b82b0d0fc5597889";
+    fetch(queryURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+
+        setlatlon({lat:lat, lon:lon});
+
+        console.log("Got lat/lon");
+
+        ReactDOM.createRoot(document.getElementById('root')).render(
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>,
+        );        
+      });
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Events getLatLon={getLatLon}/>
+      <MapTool latlon={latlon}/>
     </>
   )
 }
---
+++ src/Components/Map/Map.jsx
@@ -0,0 +1,25 @@
+// import React from 'react';
+
+// import Map from 'ol/Map.js';
+// import View from 'ol/View.js';
+// import TileLayer from 'ol/layer/Tile.js';
+// import OSM from 'ol/source/OSM.js';
+
+// const map = new Map({
+//   view: new View({
+//     center: [0, 0],
+//     zoom: 1,
+//   }),
+//   layers: [
+//     new TileLayer({
+//       source: new OSM(),
+//     }),
+//   ],
+//   target: 'map',
+// });
+
+// export default function MapTool () {
+//     return (
+//         <div id="map" class="map" tabIndex="0"></div>
+//     )
+// }
