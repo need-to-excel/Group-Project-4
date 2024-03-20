b59880ddb5127aa24a311dbd9f3b3c9702f8f057
nickncb96 <nicholasncb12@hotmail.com>
2024-03-20 21:44:52 +0000

    updated the home component so it appears more welcoming  by adding text and a explore button which take the user to the events to view before having to log inn or sign up

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,55 @@
 import React from 'react';
 import { Typography, Button, Grid } from '@mui/material';
 import { Link } from 'react-router-dom'; 
+import GoogleMapReact from 'google-map-react';
 
 const Home = () => {
   
   return (
-<h1>This is the home page</h1>
+    <Grid container spacing={2} justifyContent="center">
+      <Grid item xs={12}>
+        <Typography variant="h4">Welcome to Small Eventz!</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="body1">
+          Small Eventz is your go-to platform for finding social events and meeting new people in your area. Whether you're looking for a jogging partner, group exercise, stand-up comedy, or just someone to hang out with, we've got you covered.
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Button component={Link} to="/explore" variant="contained" color="primary">
+          Explore Events
+        </Button>
+      </Grid>
+      <Grid item xs={12} style={{ height: '400px', width: '100%' }}>
+        <InteractiveMap />
+      </Grid>
+    </Grid>
   );
 };
+
+const InteractiveMap = () => {
+    const events = []; 
+    
+   
+    const mapOptions = {
+        center: { lat: 40.7128, lng: -74.0060 }, 
+        zoom: 10, 
+    };
+
+    return (
+        <div className="interactive-map">
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'YKBXDTX4IKSXEOITVZOK' }} 
+                defaultCenter={mapOptions.center}
+                defaultZoom={mapOptions.zoom}
+            >
+                {events.map(event => (
+                    <Marker key={event.id} lat={event.lat} lng={event.lng} />
+                ))}
+            </GoogleMapReact>
+        </div>
+    );
+}
 
 export default Home;
 
