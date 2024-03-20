5c8cc696ef79391e05c96e1b601d94774638b396
nickncb96 <nicholasncb12@hotmail.com>
2024-03-20 21:04:06 +0000

    created a page for events component and also updates the App.jsx by adding  routing for Explore, Profile, and Events pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
-import Signup from './pages/signup';
-import Login from './pages/login';
 import Explore from './pages/explore';
-import './App.css';
+import Profile from './pages/profile';
+import Events from './pages/events.jsx';
 import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
 
 function App() {
-  return (
+    // State variable to track login status
+    const [loggedIn, setLoggedIn] = useState(false);
 
-    <div>
-      <Navbar />
-      <div className="content">
+    // Function to toggle login status
+    const toggleLogin = () => {
+        setLoggedIn(!loggedIn);
+    };
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/explore" element={<Explore />} />
-        </Routes>
-
-      </div>
-    </div>
-  );
+    return (
+        <>
+            {/* Pass the loggedIn state variable to the Navbar component */}
+            <Navbar loggedIn={loggedIn} />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/explore" element={<Explore />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/events" element={<Events />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/events.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const Events = () => {
+  return (
+    <div>
+      <Typography variant="h4">Events</Typography>
+      {/* Add list of events and functionality here */}
+    </div>
+  );
+};
+
+export default Events;
+
