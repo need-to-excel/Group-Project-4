60066f98ca5fbd005f53f0bdf4cb3be7e91eeb06
teddwill123 <teddywilliejr@gmail.com>
2024-03-21 23:58:29 +0530

    Modified the Sign-up code

--- src/Components/Signin/Signin.jsx
+++ src/Components/Signin/Signin.jsx
@@ -5,12 +5,20 @@ import emailIcon from "../../assets/email.png";
 import personIcon from "../../assets/person.png";
 import passwordIcon from "../../assets/password.png";
 import { Paper } from "@mui/material";
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { Center } from "@chakra-ui/layout";
+
 
 const Signin = () => {
     const [action, setAction] = useState("Sign Up");
 
     return (
         <Paper className="container">
+            <div className="submit-container">
+                <div className={action === "Login" ? "submit gray" : "submit"}onClick={() => {setAction("Sign Up");}}>Sign Up</div>
+                <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => {setAction("Login");}}>Login</div>
+            </div>
             <div className="header">
                 <div className="text">{action}</div>
                 <div className="underline"></div>
@@ -32,10 +40,9 @@ const Signin = () => {
                 </div>
             </div>
             {action === "Sign Up" ? <div></div>:<div className="forget-password">Lost Password? <span>Click Here</span></div>}
-            <div className="submit-container">
-                <div className={action === "Login" ? "submit gray" : "submit"}onClick={() => {setAction("Sign Up");}}>Sign Up</div>
-                <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => {setAction("Login");}}>Login</div>
-            </div>
+            <Stack spacing={2} direction="row" justifyContent= "Center" marginTop= "30px">
+                <Button href="#text-buttons" variant="contained" color="primary">{action}</Button>
+            </Stack>
         </Paper>
     );
 };
